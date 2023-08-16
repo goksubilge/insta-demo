99a9dcde7bc6abe97a43dc21c9ee19e23f471403
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-08-16 21:08:49 +0300

    add: r-hook-form, warnings text

--- src/compo/Login.js
+++ src/compo/Login.js
@@ -2,36 +2,61 @@ import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="max-w[480px]">
-      <h1 className="flex  align-middle justify-center text-3xl font-bold text-red-700">
+    <div className="max-w-[280px] mx-auto">
+      <h1 className="flex  align-middle justify-center text-3xl font-bold text-stone-500">
         Gir artık !
       </h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label className="flex flex-col gap-2 py-2">
-          <span>E-Mail</span>
+          <span className="flex justify-between items-baseline">
+            E-Mail
+            {errors.email && (
+              <span className="text-red-500 text-xs ">
+                {" "}
+                *Warning! This field is required
+              </span>
+            )}
+          </span>
           <input
-            className="flex-[2] p-2 round rounded-lg boorder border-red-600  "
+            className=" p-2 round rounded-lg boorder border-red-600  "
             placeholder="mailini gir"
-            type="e-mail"
+            type="email"
+            {...register("email", { required: true })}
           />
         </label>
         <label className="flex flex-col gap-2 py-2">
-          <span>Şifre</span>
+          <span className="flex justify-between items-baseline">
+            Şifre
+            {errors.password && (
+              <span className="text-red-500 text-xs ">
+                {" "}
+                *Warning! This field is required
+              </span>
+            )}
+          </span>
           <input
-            className="flex-[2] p-2 round rounded-lg "
+            className=" p-2 round rounded-lg "
             placeholder="kodunu gir"
             type="password"
+            {...register("password", { required: true })}
           />
         </label>
-        <label className="flex flex-col gap-2 py-2  ">
+        <div className=" text-center gap-2 py-2  ">
           <button
             type="submit"
             className="bg-pink-700 rounded-lg flex-[2] p-2 text-white "
           >
             Buradan girebilirsin !
           </button>
-        </label>
+        </div>
       </form>
     </div>
   );
